95b010397038d29eda245f538c97a5688c002303
Marvindlls <marvindillenseger@gmail.com>
2024-12-23 14:26:07 +0100

    Implement location fetching with error handling and update UI accordingly

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    // verifier si le statut est granted
+    let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg("L'autorisation d'accès à votre position a été refusé");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+
+    // Refusée => message d'erreur
+
+    // Si c'est granted => variable d'état
+    setLocation(location);
+  }
+
+  let text = 'Patientez...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Caca</Text>
-      <StatusBar style="auto" />
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -18,4 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 19,
+    color: 'grey',
+  }
 });
